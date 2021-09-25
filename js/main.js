1aba0967dbc64005586fb7a6ea7fbc6f8b346559
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2021-09-25 17:47:28 -0600

    fixed the currently selected list, next need to change the heading to match

--- js/main.js
+++ js/main.js
@@ -30,6 +30,19 @@ const lists = {
         }
       ]
     },
+    3: {
+      name: "Chores",
+      todos: [
+        {
+          text: 'dishes',
+          completed: false
+        },
+        {
+          text: 'laundry',
+          completed: false
+        }
+      ]
+    },
    }
    const currentList = lists[0];
 
@@ -42,25 +55,29 @@ function render() {
       if (lists.hasOwnProperty(key)) { //Sets the name of the list and gives a id to each button
         listsHtml += `<button type="button" class="list-group-item list-group-item-action" id="list${key}" onclick="setCurrentList(list${key})">${lists[key].name}</button>`;
       }
+      
     }
     listsHtml += '</ul>';
     // print out the HTML just made
     document.getElementById('listItems').innerHTML = listsHtml;
 
+    //makes the first list the default list every time
+    document.getElementById('list1').id = 'list1' + ' currentList'
 
-    //print the current list out
-//     document.getElementById('listTitle').innerText = currentList.name;
 
-//     //get the items from the todo
-//     let todosHtml = '<ul class="list-group" id="listItems">';
-//     currentList.todos.forEach((list) => {
-//         //{todo.text}
-//     todosHtml += `<li class="list-group-item">
-//     <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
-//     {todo.text}</li>`;
-//     // print the todos
-//     document.getElementById('current-list-todos').innerHTML = todosHtml;
-//  });
+    //print the current list name in the heading
+    document.getElementById('listTitle').innerText = currentList[key].name; //FIX THIS NEXT
+
+    //get the items from the todo
+    let todosHtml = '<ul class="list-group" id="listItems">';
+    currentList.todos.forEach((list) => {
+        //{todo.text}
+    todosHtml += `<li class="list-group-item">
+    <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+    {todo.text}</li>`;
+    // print the todos
+    document.getElementById('current-list-todos').innerHTML = todosHtml;
+ });
 
 }
 render()
@@ -83,18 +100,25 @@ function addTodo() {
 function setCurrentList(identity) {
   let listId = []
   for (const key in lists) {
-    if (lists.hasOwnProperty(key)) { //Sets the name of the list and gives a id to each button
+    if (lists.hasOwnProperty(key)) { //Gets the name of the lists and gives a id to each button
       listId.push('list' + key)
     }
     
   }
   let selectedList = identity.id
-  let tobeRemoved = listId.indexOf(selectedList) //removes the chosed list from the listId
-  console.log(tobeRemoved)
-  listId.splice(tobeRemoved, 1)
-  console.log(listId)
 
-//create a loop to go through and reset the other lists
-
-  identity.id += ' currentList'
+  let revisedList  = '<ul class="list-group" id="listItems">';
+ for (let numbList in lists) {
+  
+  //same as in render function to go through the buttons
+    revisedList += `<button type="button" class="list-group-item list-group-item-action" id="list${numbList}" onclick="setCurrentList(list${numbList})">${lists[numbList].name}</button>`;
+    
+ }
+ console.log(selectedList)
+  revisedList += '</ul>';
+  document.getElementById('listItems').innerHTML = revisedList;
+  
+  //changes the id so it is the current list
+  let currentId = identity.id
+  document.getElementById(selectedList).id = currentId + ' currentList'
 }
