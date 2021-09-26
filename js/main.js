6a4cce355b6c727343e8e343ef88ba6e77271f07
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2021-09-26 15:44:00 -0600

    started the addItem function but ran into some problems

--- js/main.js
+++ js/main.js
@@ -60,9 +60,9 @@ function render() {
     document.getElementById('listItems').innerHTML = listsHtml;
 
 }
-render()
-showListItems(1)
-setCurrentList(1)
+render();
+showListItems(1);
+setCurrentList(1);
 
 //creates a varable to keep track of the length of the number of lists
 let todoListLength = Object.keys(lists).length;
@@ -71,11 +71,11 @@ function addList() {
     let newListName = document.getElementById('toDoInput').value;
     if (newListName) {
       //adds 1 to the length of the todoListLength to properly add another one
-      todoListLength += 1
+      todoListLength += 1;
       //creates a new object in the list of objects and initialzes the name given and an empty todo list
       lists[todoListLength] = {'name': newListName, 'todos': []};
       //sets the newly created list as the current list
-      setCurrentList(todoListLength)
+      setCurrentList(todoListLength);
       }
     }
 
@@ -91,25 +91,32 @@ function setCurrentList(identity) {
   //changes motifies the chosen ID so that it is the current list
   document.getElementById(theChosen).id = theChosen + ' currentList';
   //adds an active class to the list button to make it blue when selected
-  document.getElementById(theChosen + ' currentList').classList.add('active')
+  document.getElementById(theChosen + ' currentList').classList.add('active');
   //calls the showListItems function to show the items for the chosen list
-  showListItems(identity)
+  showListItems(identity);
 }
 
 
 function showListItems(listNum) {
   //similar to the render function but displays the todo items for each specific list
-  let listItemHtml = '<ul class="list-group" id="listItems">'
+  let listItemHtml = '<ul class="list-group" id="listItems">';
   //opens up the chosen list and gets the todo list
-  let items = lists[listNum]['todos']
-  let itemNum = 0
-  let currentItem = ''
+  let items = lists[listNum]['todos'];
+  let itemNum = 0;
+  let currentItem = '';
   for (const key in items) {
     //this loop goes through each item in the list and adds a button for it
-   currentItem =items[key]['text']
-   listItemHtml +=  `<li class="list-group-item"><input class="form-check-input me-1" type="checkbox" value="" aria-label="...">${currentItem}</li>`
+   currentItem =items[key]['text'];
+   listItemHtml +=  `<li class="list-group-item"><input class="form-check-input me-1" type="checkbox" value="" aria-label="...">${currentItem}</li>`;
   }
-  listItemHtml += '</ul>'
+  listItemHtml += '</ul>';
   //displays to the item section
   document.getElementById('todoItems').innerHTML = listItemHtml;
+}
+
+
+addItem()
+function addItem() {
+  let currentList = (document.getElementById('currentList'));
+  console.log(currentList);
 }
