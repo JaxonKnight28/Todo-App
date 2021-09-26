38fff70f287dcfeec40fc42ffaf218484a20d8d1
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2021-09-26 14:57:01 -0600

    Major changes on the main functions. I improved them alot and made them work together much better
    copied the old code into the oldStuff file to keep in case I need it and so I can refer to it

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ let lists = {
       ]
     },
    }
-   const currentList = lists[0];
+   let currentList = lists[1];
 
 //renderFunction
 function render() {
@@ -53,77 +53,43 @@ function render() {
     //go through and get all the names for the different lists
     for (const key in lists) {
       if (lists.hasOwnProperty(key)) { //Sets the name of the list and gives a id to each button
-        listsHtml += `<button type="button" class="list-group-item list-group-item-action" id="list${key}" onclick="setCurrentList(list${key})">${lists[key].name}</button>`;
+        listsHtml += `<button type="button" class="list-group-item list-group-item-action" id="list${key}" onclick="setCurrentList(${key})">${lists[key].name}</button>`;
       }
-      
     }
     listsHtml += '</ul>';
     // print out the HTML just made
     document.getElementById('listItems').innerHTML = listsHtml;
 
-    //makes the first list the default list every time
-    document.getElementById('list1').id = 'list1' + ' currentList'
-    document.getElementById('listTitle').innerText = lists[1].name;
-
-
-
-//     //get the items from the todo
-//     let todosHtml = '<ul class="list-group" id="listItems">';
-//     currentList.todos.forEach((list) => {
-//         //{todo.text}
-//     todosHtml += `<li class="list-group-item">
-//     <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
-//     {todo.text}</li>`;
-//     // print the todos
-//     document.getElementById('current-list-todos').innerHTML = todosHtml;
-//  });
-
 }
 render()
-// the function to add todos
+setCurrentList(1)
 
-let = todLlistLength = Object.keys(lists).length;
+//creates a varable to keep track of the length of the number of lists
+let todoListLength = Object.keys(lists).length;
 function addList() {
     //grab text from the input box
-    let text = document.getElementById('toDoInput').value;
-    if (text) {
-      todLlistLength += 1
-      //let lengthOfObject = (Object.keys(lists).length + 1);
-      lists[todLlistLength] = '';
-      
-      render()
-      document.getElementById('list' + todLlistLength).innerText = text; //This spot is broken
-    }
-    text = ''
+    let newListName = document.getElementById('toDoInput').value;
+    if (newListName) {
+      //adds 1 to the length of the todoListLength to properly add another one
+      todoListLength += 1
+      //creates a new object in the list of objects and initialzes the name given and an empty todo list
+      lists[todoListLength] = {'name': newListName, 'todos': []};
+      //sets the newly created list as the current list
+      setCurrentList(todoListLength)
+      }
     }
 
 
 
 function setCurrentList(identity) {
-  let listId = []
-  for (const key in lists) {
-    if (lists.hasOwnProperty(key)) { //Gets the name of the lists and gives a id to each button
-      listId.push('list' + key)
-    }
-    
-  }
-  let selectedList = identity.id
-
-  let selectedListNum = selectedList.replace('list', '')
-  document.getElementById('listTitle').innerText = lists[selectedListNum].name;
-
-
-  let revisedList  = '<ul class="list-group" id="listItems">';
- for (let numbList in lists) {
-  
-  //same as in render function to go through the buttons
-    revisedList += `<button type="button" class="list-group-item list-group-item-action" id="list${numbList}" onclick="setCurrentList(list${numbList})">${lists[numbList].name}</button>`;
-    
- }
-  revisedList += '</ul>';
-  document.getElementById('listItems').innerHTML = revisedList;
-  
-  //changes the id so it is the current list
-  let currentId = identity.id
-  document.getElementById(selectedList).id = currentId + ' currentList'
+  //first resets the lists by rendering it again
+  render();
+  //takes the number taken and creates a variable that matches the ID of the wanted list
+  let theChosen = 'list' + identity;
+  //changes the title of the current list
+  document.getElementById('listTitle').innerText = lists[identity].name;
+  //changes motifies the chosen ID so that it is the current list
+  document.getElementById(theChosen).id = theChosen + ' currentList';
+  //adds an active class to the list button to make it blue when selected
+  document.getElementById(theChosen + ' currentList').classList.add('active')
 }
