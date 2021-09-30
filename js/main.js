793642e11fcd58821262502bd6b98a55a2cb9fdc
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2021-09-30 09:09:10 -0600

    changed the markDone function and started the removeDone function
    removeDone is partially working but not in all situations

--- js/main.js
+++ js/main.js
@@ -4,62 +4,62 @@
 
 
 let lists = {
-    1: {
-      name: "Shopping list",
-      todos: [
-        {
-          text: 'bananas',
-          completed: false
-        },
-        {
-          text: '1 lbs ground turkey',
-          completed: false
-        }
-      ]
-    },
-    2: {
-      name: "School list",
-      todos: [
-        {
-          text: 'homework',
-          completed: false
-        },
-        {
-          text: 'math',
-          completed: false
-        }
-      ]
-    },
-    3: {
-      name: "Chores",
-      todos: [
-        {
-          text: 'dishes',
-          completed: false
-        },
-        {
-          text: 'laundry',
-          completed: false
-        }
-      ]
-    },
-   }
+  1: {
+    name: "Shopping list",
+    todos: [
+      {
+        text: 'bananas',
+        completed: false
+      },
+      {
+        text: '1 lbs ground turkey',
+        completed: false
+      }
+    ]
+  },
+  2: {
+    name: "School list",
+    todos: [
+      {
+        text: 'homework',
+        completed: false
+      },
+      {
+        text: 'math',
+        completed: false
+      }
+    ]
+  },
+  3: {
+    name: "Chores",
+    todos: [
+      {
+        text: 'dishes',
+        completed: false
+      },
+      {
+        text: 'laundry',
+        completed: false
+      }
+    ]
+  },
+}
 
 //renderFunction
 function render() {
-    //left bar html
-    let listsHtml  = '<ul class="list-group" id="listItems">';
-    //go through and get all the names for the different lists
-    for (const key in lists) {
-      if (lists.hasOwnProperty(key)) { //Sets the name of the list and gives a id to each button
-        listsHtml += `<button type="button" class="list-group-item list-group-item-action" id="list${key}" onclick="setCurrentList(${key})">${lists[key].name}</button>`;
-      }
+  //left bar html
+  let listsHtml = '<ul class="list-group" id="listItems">';
+  //go through and get all the names for the different lists
+  for (const key in lists) {
+    if (lists.hasOwnProperty(key)) { //Sets the name of the list and gives a id to each button
+      listsHtml += `<button type="button" class="list-group-item list-group-item-action" id="list${key}" onclick="setCurrentList(${key})">${lists[key].name}</button>`;
     }
-    
-    listsHtml += '</ul>';
-    listsHtml += '<button type="button" class="btn btn-danger btn-sm mt-4" onclick="removeList()">Delete Current List</button>';
-    // print out the HTML just made
-    document.getElementById('listItems').innerHTML = listsHtml;
+  }
+
+  listsHtml += '</ul>';
+  listsHtml += '<button type="button" class="btn btn-danger btn-sm mt-4" onclick="removeList()">Delete Current List</button>';
+  // print out the HTML just made
+  document.getElementById('listItems').innerHTML = listsHtml;
 
 }
 //These render the page setting the first list as the defailt
@@ -70,19 +70,19 @@ setCurrentList(1);
 //creates a varable to keep track of the length of the number of lists
 let todoListLength = Object.keys(lists).length;
 function addList() {
-    //grab text from the input box
-    let newListName = document.getElementById('toDoInput').value;
-    //clears the text input so user can easily enter another one
-    document.getElementById('toDoInput').value = '';
-    if (newListName) {
-      //adds 1 to the length of the todoListLength to properly add another one
-      todoListLength += 1;
-      //creates a new object in the list of objects and initialzes the name given and an empty todo list
-      lists[todoListLength] = {'name': newListName, 'todos': []};
-      //sets the newly created list as the current list
-      setCurrentList(todoListLength);
-      }
-    }
+  //grab text from the input box
+  let newListName = document.getElementById('toDoInput').value;
+  //clears the text input so user can easily enter another one
+  document.getElementById('toDoInput').value = '';
+  if (newListName) {
+    //adds 1 to the length of the todoListLength to properly add another one
+    todoListLength += 1;
+    //creates a new object in the list of objects and initialzes the name given and an empty todo list
+    lists[todoListLength] = { 'name': newListName, 'todos': [] };
+    //sets the newly created list as the current list
+    setCurrentList(todoListLength);
+  }
+}
 
 
 
@@ -103,7 +103,7 @@ function setCurrentList(identity) {
   globalCurrentList = identity
 }
 
-
+let uniqueID = 0
 function showListItems(listNum) {
   //similar to the render function but displays the todo items for each specific list
   let listItemHtml = '<ul class="list-group" id="listItems">';
@@ -111,11 +111,24 @@ function showListItems(listNum) {
   let items = lists[listNum]['todos'];
   let itemNum = 0;
   let currentItem = '';
+  let isDone = false;
   for (const key in items) {
     //this loop goes through each item in the list and adds a button for it
-   currentItem =items[key]['text'];
-   listItemHtml +=  `<li class="list-group-item"><input class="form-check-input me-1" type="checkbox" id="item${key}" onclick="removeItem(${key})">${currentItem}</li>`;
+    //currentItem variable is the name of the item
+    currentItem = items[key]['text'];
+    //isDone grabs if the item is completed or not
+    isDone = items[key]['completed'];
+    //statemnt checks to see if isDone is true or false, and 
+    //depending on the result it adds the checkmark so that a users
+    //checkmark is not lost when they move to other lists
+    if (isDone == false) {
+      listItemHtml += `<li class="list-group-item"><input class="form-check-input me-1" type="checkbox" id="#{key}" onclick="markDone(${key})">${currentItem}</li>`;
+    }
+    else {
+      listItemHtml += `<li class="list-group-item"><input class="form-check-input me-1" type="checkbox" id="#{key}" onclick="markDone(${key})" checked>${currentItem}</li>`;
+    }
   }
+  listItemHtml += '<button type="button" class="btn btn-danger btn-sm mt-4" onclick="removeDone()">Delete Completed Items</button>';
   listItemHtml += '</ul>';
   //displays to the item section
   document.getElementById('todoItems').innerHTML = listItemHtml;
@@ -127,19 +140,27 @@ function addItem() {
   let newItem = document.getElementById('itemInput').value;
   //clear the value from the add Item to allow to easily add another
   document.getElementById('itemInput').value = '';
-  if (newItem){
+  if (newItem) {
     //gets the todos list from the current list chosen
     let todoItems = lists[globalCurrentList]['todos'];
     // pushes the new Item to the list along with completed false
-    todoItems.push({text: newItem, completed: false});
+    todoItems.push({ text: newItem, completed: false });
     //calls the showListItems to render the added item
     showListItems(globalCurrentList);
   }
 }
 
-function removeItem(itemNum) {
-  let itemToGo = lists[globalCurrentList]['todos']
-  lists[globalCurrentList]['todos'].splice(itemNum, 1)
+function markDone(itemNum) {
+  //checks to see if the item selected is false
+  if (lists[globalCurrentList]['todos'][itemNum]['completed'] == false) {
+    //if it is false it changes it to true
+    lists[globalCurrentList]['todos'][itemNum]['completed'] = true
+  }
+  else {
+    //if it is true it changes it back to false so the user can select and deselect items
+    lists[globalCurrentList]['todos'][itemNum]['completed'] = false
+  }
+  //shows the list and keeps the checkmarks that the user has completed
   showListItems(globalCurrentList)
 }
 
@@ -147,4 +168,18 @@ function removeList() {
   delete lists[globalCurrentList];
   document.getElementById('listTitle').innerText = ''
   render()
+}
+
+function removeDone() {
+  //creates a varaible that points to the current todo List
+  let itemsToGo = lists[globalCurrentList]['todos']
+  for (let i = 0; i < itemsToGo.length; i++) {
+    //loops through each item in the current to do list
+    if (itemsToGo[i]['completed'] == true) {
+      //if the current item in the loop is true then it removes that item
+      itemsToGo.splice(i, 1)
+    }
+  }
+  //then shows the shows the list
+  showListItems(globalCurrentList)
 }
